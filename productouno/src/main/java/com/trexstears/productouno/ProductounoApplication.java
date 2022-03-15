87db9b31d3b0d4e61f2acfbb30556693405c0c20
GerardANGP10 <gborrasa@uoc.edu>
2022-03-15 19:32:27 +0100

    First Commmit

---
+++ productouno/src/main/java/com/trexstears/productouno/ProductounoApplication.java
@@ -0,0 +1,12 @@
+package com.trexstears.productouno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductounoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductounoApplication.class, args);
+	}
+
+}
