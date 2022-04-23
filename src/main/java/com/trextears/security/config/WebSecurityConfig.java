81e94cf25771541af305ca864386651bffdba10f
GerardANGP10 <gborrasa@uoc.edu>
2022-04-23 15:03:20 +0200

    Add WebScurity

--- src/main/java/com/trextears/security/config/WebSecurityConfig.java
+++ src/main/java/com/trextears/security/config/WebSecurityConfig.java
@@ -3,35 +3,53 @@ package com.trextears.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig{
 
     /**
      * Solo se permiten peticiones autenticadas con el tipo HTTPBasic
      * @param http
      * @throws Exception
      */
-   @Override
-    protected void configure(HttpSecurity http) throws Exception{
-       http.csrf().disable()
-               .authorizeRequests().anyRequest().authenticated()
-               .and()
-               .httpBasic();
+   @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+       http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
+       return http.build();
    }
 
+    /**
+     * Creamos una instancia de el BcryptPassword encoder para encriptar y desencrptar las contraseñàs
+     * @return
+     */
+   @Bean
+   public PasswordEncoder encoder(){
+       return new BCryptPasswordEncoder();
+   }
+
+
+    /**
+     * Generamos un usuario en memoria con la contraseña encriptada(Bcrypt) y un rol de admin
+     * @param auth
+     * @throws Exception
+     */
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
        auth.inMemoryAuthentication()
-               .withUser("¨Gerard")
-               .password("password")
-               .roles("USER");
+               .withUser("admin")
+               .password("$2a$12$657yl7p643ZMqs5uhl9VIOEixF6SJzZ8QgYLMqdV5FVcZVkJg.3AK").roles("admin");
   }
 }
